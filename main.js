5fec2b41fbdb750d59f917144e22814de3fd3a45
Vidyuth-droid <72655577+Vidyuth-droid@users.noreply.github.com>
2021-05-01 16:39:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("This is my first console project, hope u like it :)")
+var name= "Vidyuth"
+var last_name= "Venkatesh"
+console.log(name);
+console.log(last_name);
+var full=name.concat(last_name);
+console.log(full);
+var n1=45;
+var n2=3;
+console.log(n1);
+console.log(n2);
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
